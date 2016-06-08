4440fad422f1519f1020b4286392a847f22012ab
Patrick Conroy <patrickconroy@Patricks-MacBook-Air.local>
2016-06-08 14:23:13 -0500

    Feature Complete

--- best.R
+++ best.R
@@ -1,17 +1,25 @@
 best <- function(state, outcome) {
     
     if(!is.element(state, state.abb)) {
-        errmsg <- sprintf("Error in best(\"%s\", \"%s\") : invalid state", state, outcome)
-        stop(errmsg)
+        stop("invalid state")
     }
+    
+    conditions <- c("heart attack", "heart failure", "pneumonia")
+    
+    cidx <- match(outcome, conditions)
+    if(is.na(cidx)) {
+        stop("invalid outcome")
+    }
+    
+    tidx <- c(11, 17, 23)[cidx]
     
     db <- read.csv("data/outcome-of-care-measures.csv")
     
     hospitals <- db[db[,7] == state,] # get just the hospitals in our state
     
-    hospitals[,11] <- as.numeric(hospitals[,11]) # make the rate numeric so . . .
-    best_rate <- min(hospitals[,11]) # we can find the best rate.
-    best_list <- hospitals[hospitals[,11] == best_rate,] # make a list of "best" hospitals
+    hospitals[,tidx] <- as.numeric(hospitals[,tidx]) # make the rate numeric so . . .
+    best_rate <- min(hospitals[,tidx]) # we can find the best rate.
+    best_list <- hospitals[hospitals[,tidx] == best_rate,] # make a list of "best" hospitals
     sorted <- best_list[order(best_list[,'Hospital.Name']),] # bug here
     as.character(sorted[1,'Hospital.Name'])
 }
