582b865a693476df1f04fb079ff3f311f8c96e62
Patrick Thomas Conroy, Jr <conroy@ptconroy.com>
2016-06-08 22:06:05 -0500

    Bug squashing & commenting

--- best.R
+++ best.R
@@ -1,4 +1,12 @@
+##
+## best returns the hospital name for the lowest mortality rate for an outcome in the state
+##  - state: the two letter abbreviation for a state (from state.abb)
+##  - outcome: must be "heart attack", "heart failure", "pneumonia"
+##
+
 best <- function(state, outcome) {
+    
+    # check for valid state & outcome
     
     if(!is.element(state, state.abb)) {
         stop("invalid state")
@@ -6,20 +14,25 @@ best <- function(state, outcome) {
     
     conditions <- c("heart attack", "heart failure", "pneumonia")
     
-    cidx <- match(outcome, conditions)
+    cidx <- match(outcome, conditions) # cidx identifies which outcome we want
     if(is.na(cidx)) {
         stop("invalid outcome")
     }
     
-    tidx <- c(11, 17, 23)[cidx]
+    tidx <- c(11, 17, 23)[cidx] # tidx is the table column index for the desired outcome
     
     db <- read.csv("data/outcome-of-care-measures.csv")
     
     hospitals <- db[db[,7] == state,] # get just the hospitals in our state
     
-    hospitals[,tidx] <- as.numeric(hospitals[,tidx]) # make the rate numeric so . . .
-    best_rate <- min(hospitals[,tidx]) # we can find the best rate.
+    # the data frame columns holding our mortality rates are factors
+    # we need to convert them to numeric, silencing the "NAs introduced by coercion" warning,
+    # so that . . .
+    
+    hospitals[,tidx] <- suppressWarnings(as.numeric(as.character(hospitals[,tidx])))
+    best_rate <- min(hospitals[,tidx], na.rm = TRUE) # . . . we can find the best rate.
+    
     best_list <- hospitals[hospitals[,tidx] == best_rate,] # make a list of "best" hospitals
-    sorted <- best_list[order(best_list[,'Hospital.Name']),] # bug here
+    sorted <- best_list[order(best_list[,'Hospital.Name']),] # for ties, first in alphabet wins
     as.character(sorted[1,'Hospital.Name'])
 }
